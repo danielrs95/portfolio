d0f4ebf48175d750ae0fddc85428ab65a9f3c118
Daniel <danielrs9504@gmail.com>
2022-08-27 09:05:03 -0500

    Adds fullpagejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
+import ReactFullpage from "@fullpage/react-fullpage";
 import React from "react";
 
 function App() {
+  const anchors = ["firstPage", "secondPage", "thirdPage"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactFullpage
+      anchors={anchors}
+      navigation
+      navigationTooltips={anchors}
+      // sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+      render={() => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <h3>Section 1</h3>
+            </div>
+            <div className="section">
+              <h3>Section 2</h3>
+            </div>
+            <div className="section">
+              <h3>Section 3</h3>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   );
 }
 
