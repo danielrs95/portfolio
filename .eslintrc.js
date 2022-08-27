82d740b4fab44cdc5ab02bbc99cd72912cdcdd6a
Daniel <danielrs9504@gmail.com>
2022-08-27 08:50:55 -0500

    Initial commit, setting basic structure for app

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true, // Browser global variables like `window` etc.
+    commonjs: true, // CommonJS global variables and CommonJS scoping.Allows require, exports and module.
+    es6: true, // Enable all ECMAScript 6 features except for modules.
+    jest: true, // Jest global variables like `it` etc.
+    node: true, // Defines things like process.env when generating through node
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "airbnb",
+    "plugin:import/typescript",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser", // Uses babel-eslint transforms.
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+  },
+  plugins: [
+    "react",
+    "react-hooks",
+    "@typescript-eslint/eslint-plugin",
+    "@typescript-eslint",
+    "prettier",
+  ],
+  root: true, // For configuration cascading.
+  rules: {
+    "react/jsx-filename-extension": "off",
+    "import/extensions": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/function-component-definition": "off",
+  },
+  settings: {
+    react: {
+      version: "detect", // Detect react version
+    },
+  },
+};
