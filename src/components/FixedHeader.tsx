3fb3293f609068aada823e9bddc95e7a57e39ac8
Daniel <danielrs9504@gmail.com>
2022-08-28 06:14:40 -0500

    Styles on header

--- src/components/FixedHeader.tsx
+++ src/components/FixedHeader.tsx
@@ -1,8 +1,10 @@
 import {
+  CloseOutlined,
   CodeOutlined,
   GithubOutlined,
   HomeOutlined,
   LinkedinOutlined,
+  MenuOutlined,
   UserOutlined,
 } from "@ant-design/icons";
 import { Col, Layout, Menu, Row } from "antd";
@@ -11,40 +13,44 @@ import stylesFixedHeader from "../styles/FixedHeader.module.css";
 
 const { Header } = Layout;
 
+const menuItems = [
+  { label: <a href="#Home">Home</a>, key: "Home", icon: <HomeOutlined /> },
+  { label: <a href="#About">About</a>, key: "About", icon: <UserOutlined /> },
+  { label: <a href="#Work">Work</a>, key: "Work", icon: <CodeOutlined /> },
+  {
+    label: (
+      <a
+        href="https://www.linkedin.com/in/dramirezs95/?locale=en_US"
+        target="_blank"
+        rel="noreferrer"
+      >
+        LinkedIn
+      </a>
+    ),
+    key: "LinkedIn",
+    icon: <LinkedinOutlined />,
+  },
+  {
+    label: (
+      <a href="https://github.com/danielrs95" target="_blank" rel="noreferrer">
+        GitHub
+      </a>
+    ),
+    key: "GitHub",
+    icon: <GithubOutlined />,
+  },
+];
+
 const FixedHeader: React.FC = () => (
   <Header className={stylesFixedHeader.layout}>
     <Row align="middle" justify="center">
       <Col xs={20} className={stylesFixedHeader.headerContainer}>
         <div>Daniel</div>
-        <div style={{ display: "flex" }}>
-          <Menu
-            mode="horizontal"
-            className={stylesFixedHeader.menu}
-            activeKey={undefined}
-          >
-            <Menu.Item icon={<HomeOutlined />}>
-              <a href="#Home">Home</a>
-            </Menu.Item>
-            <Menu.Item icon={<UserOutlined />}>
-              <a href="#About">About</a>
-            </Menu.Item>
-            <Menu.Item icon={<CodeOutlined />}>
-              <a href="#Work">Work</a>
-            </Menu.Item>
-          </Menu>
-          <LinkedinOutlined
-            className={stylesFixedHeader.socialIcons}
-            onClick={() =>
-              window.open(
-                "https://www.linkedin.com/in/dramirezs95/?locale=en_US"
-              )
-            }
-          />
-          <GithubOutlined
-            className={stylesFixedHeader.socialIcons}
-            onClick={() => window.open("https://github.com/danielrs95")}
-          />
-        </div>
+        <Menu
+          overflowedIndicator={<MenuOutlined />}
+          items={menuItems}
+          mode="horizontal"
+        />
       </Col>
     </Row>
   </Header>
